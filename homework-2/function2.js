6b352c0ecd248b0c5adc6706469f36425c800671
Pero Pandiloski <pero.pandiloski@gmail.com>
2020-11-22 23:45:02 +0100

    Homework 2 - functions

---
+++ homework-2/function2.js
@@ -0,0 +1,15 @@
+let words = ['Македонија','анаволимилована','дај ми една чаша полна со вино','радар','кајак', 'калабалак','нели не си ти сенилен','јадење и пиење дај','слушател','фудбал','музика'];
+
+function palindromeCheck(words) {
+    let palindromes = [];
+
+    for (let i = 0; i < words.length; i++) {
+        let p = words[i].split("").reverse().join("");
+        if (p === words[i]) {
+            palindromes.push(words[i]);
+        }
+    }
+
+    console.log(palindromes);
+}
+palindromeCheck(words);
